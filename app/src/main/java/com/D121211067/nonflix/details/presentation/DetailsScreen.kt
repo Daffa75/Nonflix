46942a538796be0af86fe05b597768b73a401f89
Daffa Yuslim <andidaffa75@gmail.com>
2023-12-14 23:00:06 +0800

    fix: movie detail ui improvement

--- app/src/main/java/com/D121211067/nonflix/details/presentation/DetailsScreen.kt
+++ app/src/main/java/com/D121211067/nonflix/details/presentation/DetailsScreen.kt
@@ -2,6 +2,7 @@ package com.D121211067.nonflix.details.presentation
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -25,7 +26,6 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
@@ -33,7 +33,6 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.navigation.NavBackStackEntry
 import coil.compose.AsyncImagePainter
 import coil.compose.rememberAsyncImagePainter
 import coil.request.ImageRequest
@@ -136,55 +135,78 @@ fun DetailsScreen() {
 
             detailsState.movie?.let { movie ->
                 Column(
-                    modifier = Modifier.fillMaxWidth()
+                    modifier = Modifier
+                        .fillMaxWidth()
                 ) {
+
                     Text(
                         modifier = Modifier.padding(start = 16.dp),
                         text = movie.title,
-                        fontSize = 19.sp,
-                        fontWeight = FontWeight.SemiBold
+                        color = MaterialTheme.colorScheme.onSurface,
+                        fontWeight = FontWeight.SemiBold,
+                        fontSize = 19.sp
                     )
 
-                    Spacer(modifier = Modifier.height(16.dp))
+                    Spacer(modifier = Modifier.height(12.dp))
 
                     Row(
-                        modifier = Modifier
-                            .padding(start = 16.dp)
+                        verticalAlignment = Alignment.CenterVertically
                     ) {
                         RatingBar(
+                            modifier = Modifier.padding(start = 16.dp),
                             starsModifier = Modifier.size(18.dp),
-                            rating = movie.vote_average / 2
+                            rating = movie.vote_average.div(2)
                         )
 
                         Text(
-                            modifier = Modifier.padding(start = 4.dp),
+                            modifier = Modifier.padding(
+                                horizontal = 4.dp
+                            ),
                             text = movie.vote_average.toString().take(3),
-                            color = Color.LightGray,
                             fontSize = 14.sp,
-                            maxLines = 1,
+                            color = MaterialTheme.colorScheme.onSurface,
                         )
                     }
 
-                    Spacer(modifier = Modifier.height(12.dp))
+                    Spacer(modifier = Modifier.height(6.dp))
+
+                    Row(verticalAlignment = Alignment.CenterVertically) {
+
+
+                        Text(
+                            modifier = Modifier.padding(start = 16.dp),
+                            text =
+                            if (movie.release_date != "Unavailable")
+                                movie.release_date.take(4)
+                            else "",
+
+                            color = MaterialTheme.colorScheme.onSurface,
+                            fontSize = 15.sp
+                        )
+
+                        Spacer(modifier = Modifier.width(8.dp))
+
+                        Text(
+                            modifier = Modifier
+                                .border(
+                                    width = 1.dp,
+                                    color = MaterialTheme.colorScheme.onSurface,
+                                    shape = RoundedCornerShape(6.dp)
+                                )
+                                .padding(horizontal = 6.dp, vertical = 0.5.dp),
+                            text = if (movie.adult) "+18" else "-12",
+                            color = MaterialTheme.colorScheme.onSurface,
+                            fontSize = 12.sp
+                        )
+                    }
+
+                    Spacer(modifier = Modifier.height(6.dp))
 
                     Text(
                         modifier = Modifier.padding(start = 16.dp),
                         text = stringResource(R.string.language) + movie.original_language
                     )
 
-                    Spacer(modifier = Modifier.height(10.dp))
-
-                    Text(
-                        modifier = Modifier.padding(start = 16.dp),
-                        text = "Release Date: " + movie.release_date
-                    )
-
-                    Spacer(modifier = Modifier.height(10.dp))
-
-                    Text(
-                        modifier = Modifier.padding(start = 16.dp),
-                        text = movie.release_date + stringResource(R.string.votes)
-                    )
                 }
             }
         }
