a09b1f26868a73ad4356ffca9e0988a70fb452fe
Daffa Yuslim <andidaffa75@gmail.com>
2023-12-14 10:28:53 +0800

    feat: dependency injection

---
+++ app/src/main/java/com/D121211067/nonflix/di/RepositoryModule.kt
@@ -0,0 +1,21 @@
+package com.D121211067.nonflix.di
+
+import com.D121211067.nonflix.movieList.data.repository.MovieListRepositoryImpl
+import com.D121211067.nonflix.movieList.domain.repository.MovieListRepository
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class RepositoryModule {
+
+    @Binds
+    @Singleton
+    abstract fun bindMovieListRepository(
+        movieListRepositoryImpl: MovieListRepositoryImpl
+    ): MovieListRepository
+
+}
